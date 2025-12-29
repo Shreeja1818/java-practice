7074fe83ff22005f7e662ba6ea0537f8b87be80a
Shreeja1818 <sshreeja1818@gmail.com>
2025-12-30 00:06:45 +0530

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,25 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+        char operator = '+';
+
+        switch (operator) {
+            case '+':
+                System.out.println("Result = " + (a + b));
+                break;
+            case '-':
+                System.out.println("Result = " + (a - b));
+                break;
+            case '*':
+                System.out.println("Result = " + (a * b));
+                break;
+            case '/':
+                System.out.println("Result = " + (a / b));
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
